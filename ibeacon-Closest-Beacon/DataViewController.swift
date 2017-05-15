8906560aa1e62013776c9e6e33d4ff395628658e
cast <cast@windows.connectify>
2017-05-15 18:55:19 +0800

    Initial Commit

---
+++ ibeacon-Closest-Beacon/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  ibeacon-Closest-Beacon
+//
+//  Created by cast on 15/05/2017.
+//  Copyright © 2017 JTAC. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
